bfb950bfc25901fb01d316d246154cde02b19bd1
knightliao <knightliao@gmail.com>
2016-06-21 20:36:56 +0800

    init

---
+++ src/main/java/com/github/knightliao/support/demo/SampleWebFreeMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.github.knightliao.support.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebFreeMarkerApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleWebFreeMarkerApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/templates/welcome.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<body>
+	Dadte32: ${data.time ? time}
+
+    data message: ${data.message }
+
+</body>
+
+</html>
